39c676ba9f83a914342511a077165161e0c8a400
Vinicius Moreira <vmcruz89@gmail.com>
2024-12-19 21:24:37 -0300

    Create models, migrations, migrate and create admin user

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
-# Register your models here.
+class Estudantes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'numero_celular')
+    list_display_links = ('id', 'nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(Estudante, Estudantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links=('id', 'codigo',)
+    search_fields=('codigo',)
+
+admin.site.register(Curso, Cursos)
